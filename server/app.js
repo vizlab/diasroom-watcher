64d564e57f5b8081df43d9a0e8a9714fc0ae031f
Yosuke ONOUE <onoue@likr-lab.com>
2014-08-29 16:59:26 +0900

    Remove console output

--- server/app.js
+++ server/app.js
@@ -4,16 +4,11 @@ var io = require('socket.io')();
 io.listen(9000)
 
 setInterval(function(){
-	console.log('read temperature');
 	gpio(0x68 <<8)
 		.then(function(result){
 			var level =result & 0x3ff;
 			var volt = 3300 *level/1024;
 			var temperature = (volt - 500) /10;
-			console.log(temperature); 
 			io.emit('emit_from_server', '' + temperature);
 		});
-}, interval);	
-// io.on('connect', function() {
-// 	console.log(arguments);
-// });
+}, interval);
